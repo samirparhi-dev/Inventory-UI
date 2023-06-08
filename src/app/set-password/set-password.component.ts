d52ea472f3ce277348d8a40bc156edce56b9b475
Parvesh Kumar <pa20456447@wipro.com>
2023-06-08 16:34:37 +0530

    passwordEcriptionCnages

--- src/app/set-password/set-password.component.ts
+++ src/app/set-password/set-password.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../login/authentication.service';
 import { ConfirmationService } from '../app-modules/core/services/confirmation.service';
-
+import * as CryptoJS from 'crypto-js';
 @Component({
 	selector: 'app-set-password',
 	templateUrl: './set-password.component.html',
@@ -14,13 +14,79 @@ export class SetPasswordComponent {
 	confirmpwd: any;
 	uname: any;
 	dynamictype: any = "password";
+	key: any;
+  iv: any;
+  SALT: string = "RandomInitVector";
+  Key_IV: string = "Piramal12Piramal";
+  encPassword: string;
+  _keySize: any;
+  _ivSize: any;
+  _iterationCount: any;
+  encryptedConfirmPwd : any;
+  password: any;
+
 
 	constructor(
 		private router: Router,
 		private authService: AuthenticationService,
-		private confirmationService: ConfirmationService) { }
+		//private confirmationService: ConfirmationService) { }
 
-	ngOnInit() {
+		private confirmationService: ConfirmationService) {
+			this._keySize = 256;
+            this._ivSize = 128;
+            this._iterationCount = 1989;
+		 }
+
+
+		 get keySize() {
+			return this._keySize;
+		  }
+		
+		  set keySize(value) {
+			this._keySize = value;
+		  }
+		
+		
+		
+		  get iterationCount() {
+			return this._iterationCount;
+		  }
+		
+		
+		
+		  set iterationCount(value) {
+			this._iterationCount = value;
+		  }
+		
+		
+		
+		  generateKey(salt, passPhrase) {
+			return CryptoJS.PBKDF2(passPhrase, CryptoJS.enc.Hex.parse(salt), {
+			  keySize: this.keySize / 32,
+			  iterations: this._iterationCount
+			})
+		  }
+		
+		
+		
+		  encryptWithIvSalt(salt, iv, passPhrase, plainText) {
+			let key = this.generateKey(salt, passPhrase);
+			let encrypted = CryptoJS.AES.encrypt(plainText, key, {
+			  iv: CryptoJS.enc.Hex.parse(iv)
+			});
+			return encrypted.ciphertext.toString(CryptoJS.enc.Base64);
+		  }
+		
+		  encrypt(passPhrase, plainText) {
+			let iv = CryptoJS.lib.WordArray.random(this._ivSize / 8).toString(CryptoJS.enc.Hex);
+			let salt = CryptoJS.lib.WordArray.random(this.keySize / 8).toString(CryptoJS.enc.Hex);
+			let ciphertext = this.encryptWithIvSalt(salt, iv, passPhrase, plainText);
+			return salt + iv + ciphertext;
+		  }
+		
+	
+
+		 ngOnInit() {
 		this.uname = localStorage.getItem('userID');
 	}
 
@@ -34,8 +100,11 @@ export class SetPasswordComponent {
 
 	updatePassword(new_pwd) {
 		const transactionId = this.authService.transactionId;
+		this.password = this.encrypt(this.Key_IV, new_pwd)
+		this.encryptedConfirmPwd=this.encrypt(this.Key_IV, this.confirmpwd)
+
 		if (new_pwd === this.confirmpwd) {
-			this.authService.setNewPassword(this.uname, new_pwd, transactionId)  .subscribe(
+			this.authService.setNewPassword(this.uname, this.password, transactionId)  .subscribe(
 				(response: any) => {
 				  if (response !== undefined && response !== null && response.statusCode === 200) {
 					this.successCallback();
